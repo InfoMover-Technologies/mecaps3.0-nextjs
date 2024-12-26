a1a87fdc09b5864dd8051bceee7af136e6a358a4
shakir <shakir@infomover.io>
2024-12-26 12:51:04 +0530

    Form created

--- src/app/customers/page.tsx
+++ src/app/customers/page.tsx
@@ -5,14 +5,20 @@ import {DataTable} from "primereact/datatable";
 import {Column} from "primereact/column";
 import {Button} from "primereact/button";
 import {Dialog} from "primereact/dialog";
+import {Field, Form, Formik} from "formik";
 import {InputText} from "primereact/inputtext";
 
 export default function CustomerPage() {
 
-
     const [customers, setCustomers] = useState<any[]>([])
-    const [showCreateDialog, setShowCreatDialog] = useState<boolean>(false)
+    const [showCreateDialog, setShowCreateDialog] = useState<boolean>(false)
 
+
+    const initialValues = {
+        id: '',
+        name: ''
+        // templateType: template?.templateType || 'Email'
+    };
 
     const asyncGetCustomers = async () => {
         const _customers = await getCustomersWithAPI();
@@ -27,26 +33,69 @@ export default function CustomerPage() {
     return (
         <>
             <h1>Customers</h1>
-            
+
             <Dialog onHide={() => {
-                setShowCreatDialog(false)
+                setShowCreateDialog(false)
             }}
                     visible={showCreateDialog}>
-               <div>
+                <div>
 
-                   <InputText placeholder={"Enter Id"}/>
-               </div>
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={() => {
+                            console.log("the form is submitted...")
+                        }}
+                    >
+
+                        {({handleChange, values}) => (
+                            <Form>
+                                <div className="field">
+                                    <label htmlFor="title">ID</label>
+                                    <Field name="id">
+                                        {({field}: { field: any }) => (
+                                            <InputText
+                                                id="title"
+                                                {...field}
+                                                onChange={handleChange}
+                                                value={values.id}
+
+                                            />
+                                        )}
+                                    </Field>
+                                </div>
+                                <div className="field">
+                                    <label htmlFor="title">Name</label>
+                                    <Field name="name">
+                                        {({field}: { field: any }) => (
+                                            <InputText
+                                                id="name"
+                                                {...field}
+                                                onChange={handleChange}
+                                                value={values.name}
+
+                                            />
+                                        )}
+                                    </Field>
+                                </div>
+
+
+                            </Form>
+                        )}
+
+                    </Formik>
+
+
+                </div>
 
 
             </Dialog>
-
 
 
             <div className='flex justify-content-end'>
 
                 <Button className='my-2'
                         onClick={() => {
-                            setShowCreatDialog(true)
+                            setShowCreateDialog(true)
                         }}
                         label={"Add Customer"}
                         icon={'pi pi-plus'}/>
@@ -72,7 +121,6 @@ export default function CustomerPage() {
 
                 </DataTable>
             </div>
-
 
 
         </>
